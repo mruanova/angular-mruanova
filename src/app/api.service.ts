088a7b9f4d1690e0262dcd5f3e6c03975874b85e
MAURICIO RUANOVA HURTADO <mau@MAURICIOs-MacBook-Air.local>
2017-08-27 16:17:29 -0500

    click event

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
-import 'rxjs';  // we need to import this now
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ApiService {
 
   constructor(private _http: Http) { }
 
-  search(url: string) {
+  search() {
+    const url = 'https://246gg84zg8.execute-api.us-west-2.amazonaws.com/prod/projects';
     return this._http.get(url).map(data => data.json()).toPromise();
-  };
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ let map;
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Mauricio Ruanova-Hurtado';
   projects = null;
   geocoder = null;
@@ -18,56 +18,118 @@ export class AppComponent {
   constructor(private _apiService: ApiService) {
   }
 
+  // get all the projects from the api
   ngOnInit() {
     this.initialize();
-    const url = 'https://246gg84zg8.execute-api.us-west-2.amazonaws.com/prod/projects';
-    this.promise = this._apiService.search(url);
-    if (this.promise) {
-      this.promise.then((data) => {
-        this.projects = data.Items;
-        console.log(this.projects);
-        for (var project = 0; project < this.projects.length; project++) {
-          this.addMarker(project, this.projects[project].Address);
-        }
-      })
-        .catch((err) => {
-          console.log(err);
-        });
-    } else {
-      console.log("xxx");
-    }
-  };
+    this.promise = this._apiService.search();
+    this.promise.then((data) => {
+      this.projects = data.Items.sort(function (a, b) {
+        return parseFloat(a.ProjectId) - parseFloat(b.ProjectId);
+      });
+      for (let project = 0; project < this.projects.length; project++) {
+        this.addMarker(
+          this.projects[project].ProjectId,
+          this.projects[project].Name,
+          this.projects[project].Website,
+          this.projects[project].Address);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  }
 
+  // initialize google maps
   initialize() {
     this.geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(41.8838158, -87.6415424);
-    var mapOptions = {
+    const latlng = new google.maps.LatLng(41.8838158, -87.6415424);
+    const mapOptions = {
       zoom: 14,
       center: latlng
     };
-    map = new google.maps.Map(document.getElementById('map'), mapOptions); //map
-  };
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  }
 
-  addMarker(index, address) {
+  // add a marker in google maps for each project
+  addMarker(index, title, website, address) {
+    console.log(index);
+    console.log(title);
+    console.log(website);
+    console.log(address);
     this.geocoder.geocode({ 'address': address }, function (results, status) {
-      if (status == 'OK') {
-        var marker = new google.maps.Marker({
-          position: results[0].geometry.location,
-          title: address,
+      if (status === 'OK') {
+        const marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location,
+            title: title
         });
-        marker.setMap(map);
+        // markers.push(marker);
+        const contentString = '<div id="content">' +
+            '<div class="company">' + title + '</div>' +
+            '<div id="bodyContent">' +
+            '<div class="website">' + website + '</div>' +
+            '<div class="address">' + address + '</div>' +
+            '</div>' +
+            '</div>';
+        const infowindow = new google.maps.InfoWindow({
+            content: contentString
+        });
+        if (index === 1) {
+            infowindow.open(map, marker);
+        }
         marker.addListener('click', function () {
-          var items = document.querySelector('ul').getElementsByTagName("li");
-          for (var i = 0; i < items.length; ++i) {
-            var li = items[i];
-            if (index == i) {
-              li.classList.add("yellow");
-            } else {
-              li.classList.remove("yellow");
-            }
-          }
+            infowindow.open(map, marker);
         });
       }
     });
-  };
-}
+  }
+
+  mapGeoCode(name, website, position, address) {
+    this.geocoder.geocode({ 'address': address }, function (results, status) {
+        if (status === 'OK') {
+            // set center of the map
+            map.setCenter(results[0].geometry.location);
+            const marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location,
+                title: name
+            });
+            const contentString = '<div id="content">' +
+                '<div class="link2">' + name + '</div>' +
+                '<div id="bodyContent">' +
+                '<div class="website">' + website + '</div>' +
+                '<div class="address">' + address + '</div>' +
+                '</div>' +
+                '</div>';
+            const infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+            marker.addListener('click', function () {
+                infowindow.open(map, marker);
+            });
+            infowindow.open(map, marker);
+        }
+    });
+  }
+} // class
+
+const modal = document.getElementById('myModal');
+const btn = document.getElementById('about');
+const span = document.getElementById('close');
+
+// show modal ABOUT
+btn.onclick = function () {
+    modal.style.display = 'block';
+};
+
+// hide modal ABOUT on click X
+span.onclick = function () {
+    modal.style.display = 'none';
+};
+
+// hide modal ABOUT on click anywhere outside the modal
+window.onclick = function (event) {
+    if (event.target === modal) {
+        modal.style.display = 'none';
+    }
+};
