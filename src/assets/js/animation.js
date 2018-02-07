60be9a4564179ab75c46523395efbbb2c42ec6f8
MAURICIO RUANOVA HURTADO <mau@MAURICIOs-MacBook-Air.local>
2018-02-06 21:58:44 -0600

    Angular 5

--- src/assets/js/animation.js
+++ src/assets/js/animation.js
@@ -1,11 +1,19 @@
-let arr = ["3px", "23px", "43px", "63px", "83px", "103px", "123px"],
-    el = document.getElementById('selector'),
-    i = 0;
+let tops = ["3px", "23px", "43px", "63px", "83px", "103px", "123px"],
+    photos = ["assets/img/mau.jpg","assets/img/selfie.jpg","assets/img/sunglasses.jpg"],
+    selector1 = document.getElementById('selector'),
+    photo1 = document.getElementById('photo'),
+    i = 0,
+    j = 0;
 function updateText() {
-    el.style.top = arr[i];
+    selector1.style.top = tops[i];
+    photo1.src = photos[j];
     i++;
-    if (i == arr.length) {
+    if (i == tops.length) {
         i = 0;
+        j++;
+        if (j == photos.length) {
+            j = 0;
+        }
     }
 }
 setInterval(updateText, 300);
