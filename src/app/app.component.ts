01416f19cb7b0aa1ffcae3aeb3e8a895ea196080
Mauricio Ruanova-Hurtado <mruanova@gmail.com>
2019-02-16 08:52:52 -0600

    icons and products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   title: string = 'app';
   products = [
     new Product(1, 'Angular', 0.99, '../../assets/img/angular.png'),
+    new Product(11, 'React', 0.99, '../../assets/img/react.png'),
     new Product(2, 'HTML', 0.99, '../../assets/img/html5.png'),
     new Product(3, 'CSS', 0.99, '../../assets/img/css3.png'),
     new Product(4, 'JavaScript', 0.99, '../../assets/img/js.png'),
@@ -20,10 +21,13 @@ export class AppComponent {
     new Product(8, 'Route 53', 0.99, '../../assets/img/route53.png'),
     new Product(9, 'Lambda', 0.99, '../../assets/img/lambda.png'),
     new Product(10, 'IAM', 0.99, '../../assets/img/iam.png'),
-    new Product(11, 'React', 0.99, '../../assets/img/react.png'),
-    new Product(12, 'Vue', 0.99, '../../assets/img/vue.png'),
+    new Product(16, 'DynamoDB', 0.99, '../../assets/img/dynamo.png'),
+    new Product(15, '.Net', 0.99, '../../assets/img/net.png'),
+    new Product(18, 'Azure', 0.99, '../../assets/img/azure.png'),
     new Product(13, 'Python', 0.99, '../../assets/img/python.png'),
     new Product(14, 'Ruby', 0.99, '../../assets/img/ruby.png'),
+    new Product(12, 'Vue', 0.99, '../../assets/img/vue.png'),
+    new Product(17, 'Bootstrap', 0.99, '../../assets/img/bootstrap.png'),
   ];
 
   constructor(private _productService: ProductService) {
