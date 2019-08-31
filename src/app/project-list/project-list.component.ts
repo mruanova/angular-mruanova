0555d0e602de45130468095fb74b351f741c75cb
Mauricio Ruanova-Hurtado <mruanova@gmail.com>
2019-08-31 09:08:53 -0500

    refactor-on-click

--- src/app/project-list/project-list.component.ts
+++ src/app/project-list/project-list.component.ts
@@ -7,6 +7,33 @@ import { Map } from 'mapbox-gl';
 
 let map: Map;
 
+// https://docs.mapbox.com/api/#directions
+// /directions/v5/{profile}/{coordinates}.
+const test = 'https://api.mapbox.com/directions/v5/mapbox/driving/-73.989%2C40.733%3B-74%2C40.733.json?access_token=pk.eyJ1IjoibXJ1YW5vdmEiLCJhIjoiY2p6dWs2YmcxMDVmYTNocGZ2Z2hiMDlqYiJ9.2iSMaogLhpWWMBql2_SBFg';
+/*
+# POST request to Map Matching API
+$ curl -d
+"coordinates=-117.17282,32.71204;-117.17288,32.71225;-117.17293,32.71244;-117.17292,32.71256;-117.17298,32.712603;-117.17314,32.71259;-117.17334,32.71254" "https://api.mapbox.com/matching/v5/mapbox/driving?access_token=pk.eyJ1IjoibXJ1YW5vdmEiLCJhIjoiY2p6dWs2YmcxMDVmYTNocGZ2Z2hiMDlqYiJ9.2iSMaogLhpWWMBql2_SBFg"
+x
+# POST request to Directions API with optional parameters
+$ curl -d
+"coordinates=2.344003,48.85805;2.34675,48.85727;2.34868,48.85936;2.34955,48.86084;2.34955,48.86088;2.349625,48.86102;2.34982,48.86125&steps=true&waypoints=0;6&waypoint_names=Home;Work&banner_instructions=true" "https://api.mapbox.com/directions/v5/mapbox/driving?access_token=pk.eyJ1IjoibXJ1YW5vdmEiLCJhIjoiY2p6dWs2YmcxMDVmYTNocGZ2Z2hiMDlqYiJ9.2iSMaogLhpWWMBql2_SBFg"
+*/
+/*
+http://www.liedman.net/leaflet-routing-machine/
+https://github.com/perliedman/leaflet-routing-machine
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.2.0/dist/leaflet.css" />
+<link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css" />
+<script src="https://unpkg.com/leaflet@1.2.0/dist/leaflet.js"></script>
+<script src="https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js"></script>
+L.Routing.control({
+  waypoints: [
+    L.latLng(57.74, 11.94),
+    L.latLng(57.6792, 11.949)
+  ]
+}).addTo(map);
+*/
+
 @Component({
   selector: 'project-list',
   templateUrl: './project-list.component.html',
@@ -58,7 +85,7 @@ export class ProjectListComponent implements AfterViewInit {
         // create a HTML element for each feature
         var el = document.createElement('div');
         el.className = 'marker';
-        const popup = this.createPopup(project.Name, project.Website, project.Address, project.Coordinates);
+        const popup = this.createPopup(project);
         const marker = new mapboxgl.Marker()
           .setLngLat(project.Coordinates)
           .setPopup(popup) // sets a popup on this marker
@@ -66,42 +93,34 @@ export class ProjectListComponent implements AfterViewInit {
       });
       // animate
       setTimeout(() => {
-        this.mapGeoCode(this.projects[0].Name, 
-          this.projects[0].Website, 
-          this.projects[0].Position, 
-          this.projects[0].Address, 
-          this.projects[0].Coordinates)
+        this.onClick(this.projects[0])
       }, 1500);
     });
   };
 
-  createPopup(name, website, address, coordinates) {
+  createPopup(project) {
     // make a marker for each feature and add to the map
     const contentString = '<div id="content">' +
-      '<div class="link2">' + name + '</div>' +
+      '<div class="link2">' + project.Name + '</div>' +
       '<div id="bodyContent">' +
-      '<div class="website">' + website + '</div>' +
-      '<div class="address">' + address + '</div>' +
+      '<div class="website">' + project.Website + '</div>' +
+      '<div class="address">' + project.Address + '</div>' +
       '</div>' +
       '</div>';
     var popup = new mapboxgl.Popup({ closeOnClick: false, offset: 25 })
-      .setLngLat(coordinates)
+      .setLngLat(project.Coordinates)
       .setHTML(contentString)
       .addTo(map);
     // .setText('Construction on the Washington Monument began in 1848.')
     return popup;
   };
 
-  mapGeoCode(name, website, position, address, coordinates) {
+  onClick(project) {
     if (this.envService.debug) {
-      console.log('geocode.name', name);
-      console.log('geocode.website', website);
-      console.log('geocode.position', position);
-      console.log('geocode.address', address);
-      console.log('geocode.coordinates', coordinates);
+      console.log('clicked', project);
     }
-    var popup = this.createPopup(name, website, address, coordinates);
+    var popup = this.createPopup(project);
     // Center the map on the coordinates of any clicked symbol from the 'symbols' layer.
-    map.flyTo({ center: coordinates });
+    map.flyTo({ center: project.Coordinates });
   };
 };
